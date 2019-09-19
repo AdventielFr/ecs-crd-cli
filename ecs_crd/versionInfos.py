99e60fd01d435f2f6726d6692b6d0fc5ffec6d3e
Gwendall Garnier <gwendall@MacBook-Pro-de-Gwendall.local>
2019-09-19 09:57:40 +0200

    bump version

--- ecs_crd/versionInfos.py
+++ ecs_crd/versionInfos.py
@@ -1,4 +1,4 @@
 class VersionInfos(object):
     def __init__(self):
-        self.version = '0.3.0a'
+        self.version = '0.3.0'
         self.description = 'ECS Canary Release Command LIne deploy'
