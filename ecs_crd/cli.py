04f7db1e848bad346e4217364b4f7783c9026ee6
Mikael GICQUEL (mikakev1) <mikael.public@gmail.com>
2019-09-24 17:03:21 +0200

    ecs_crd/cli.py: review

--- ecs_crd/cli.py
+++ ecs_crd/cli.py
@@ -1,20 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import click
 import os
 import logging
 import boto3
 import sys
-import re
-import codecs
 
 from ecs_crd.prepareDeploymentGlobalParametersStep import PrepareDeploymentGlobalParametersStep
 from ecs_crd.prepareUnDeploymentStep import PrepareUnDeploymentStep
 from ecs_crd.canaryReleaseInfos import CanaryReleaseInfos
 from ecs_crd.versionInfos import VersionInfos
 
+# Hum un global meriterait d'etre en MAJ
+version_infos = VersionInfos()
+
+
 class Parameters:
     def __init__(self, logger):
         self.environments = ['dev', 'qua', 'stage', 'preprod', 'prod']
-        self.regions = ['us-east-1','us-east-2','us-west-1','us-west-2','ap-east-1','ap-south-1','ap-northeast-1','ap-northeast-2','ap-northeast-3','ap-southeast-1','ap-southeast-2','ca-central-1','cn-north-1','cn-northwest-1','eu-central-1','eu-west-1','eu-west-2','eu-west-3','eu-north-1','sa-east-1','us-gov-east-1','us-gov-west-1']
+        self.regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2',
+                        'ap-east-1', 'ap-south-1', 'ap-northeast-1',
+                        'ap-northeast-2', 'ap-northeast-3', 'ap-southeast-1',
+                        'ap-southeast-2', 'ca-central-1', 'cn-north-1',
+                        'cn-northwest-1', 'eu-central-1', 'eu-west-1',
+                        'eu-west-2', 'eu-west-3', 'eu-north-1', 'sa-east-1',
+                        'us-gov-east-1', 'us-gov-west-1']
         self.environment = None
         self.region = None
         self.configuration_file = None
@@ -22,27 +33,27 @@ class Parameters:
         self.logger = logger
 
     def validate(self):
-        if self.environment == None:
+        if not self.environment:
             raise ValueError(f'environment is mandatory.')
         if self.environment not in self.environments:
             raise ValueError(f'{self.environment} is not valid environment.')
-        if self.region == None:
+        if not self.region:
             raise ValueError(f'region is mandatory.')
         if self.region not in self.regions:
             raise ValueError(f'{self.region} is not valid aws region.')
-    
-        if self.configuration_file == None:
-            if self.configuration_dir == None or self.configuration_dir == '':
+
+        if not self.configuration_file:
+            if not self.configuration_dir or self.configuration_dir == '':
                 self.configuration_dir = os.getcwd()
             if not self.configuration_dir.endswith('/'):
-                self.configuration_dir = self.configuration_dir+ '/'
+                self.configuration_dir += "/"
             if not os.path.exists(self.configuration_dir):
                 raise ValueError(f'{self.configuration_dir} not exist.')
-            
-            self.configuration_file = self.configuration_dir + self.environment + '.deploy.yml'
-    
+
+            self.configuration_file = f"{self.configuration_dir}{self.environment}.deploy.yml"
+
         if not os.path.isfile(self.configuration_file):
-            raise ValueError(f'{self.configuration_file} not exist.')
+            raise ValueError(f"{self.configuration_file} doesn't exist.")
 
         self.logger.info(''.ljust(50, '-'))
         self.logger.info(f'Step : Check parameters')
@@ -50,35 +61,36 @@ class Parameters:
         self.logger.info(f'Region             : {self.region}')
         self.logger.info(f'Environment        : {self.environment}')
         self.logger.info(f'Configuration      : {self.configuration_file}')
-        self.logger.info('') 
+        self.logger.info('')
         self.logger.info(f'Check parameters : COMPLETED')
 
-version_infos = VersionInfos()
 
 @click.group()
 def main():
-    pass 
+    pass
+
 
 @main.command(help='deploy the ECS service')
-@click.option('-e','--environment', required=True, default= 'stage', help='Environment to deploy.', show_default=True )
-@click.option('-r','--region', required=True, default= 'eu-west-3', help='Amazon Web Service region used to deploy ECS service.', show_default=True )
-@click.option('-f','--configuration-file', required=False, help='deployment configuration file.')
-@click.option('-d','--configuration-dir', required=False, help='directory to find the deployment configuration file.')
+@click.option('-e', '--environment', required=True, default='stage', help='Environment to deploy.', show_default=True)
+@click.option('-r', '--region', required=True, default='eu-west-3', help='Amazon Web Service region used to deploy ECS service.', show_default=True)
+@click.option('-f', '--configuration-file', required=False, help='deployment configuration file.')
+@click.option('-d', '--configuration-dir', required=False, help='directory to find the deployment configuration file.')
 @click.option('--verbose', is_flag=True, default=False, help='activate verbose log.')
 @click.option('--log-file', required=False, help='output log file result.')
 def deploy(
-        environment, 
-        region, 
+        environment,
+        region,
         configuration_file,
-        configuration_dir, 
-        verbose, 
+        configuration_dir,
+        verbose,
         log_file):
     logger, canary_infos = _common_action(environment, region, configuration_file, configuration_dir, verbose, log_file)
     canary_infos.action = 'deploy'
     canary_step = PrepareDeploymentGlobalParametersStep(canary_infos, logger)
-    while (canary_step != None):
+    while (canary_step):
         canary_step = canary_step.execute()
     sys.exit(canary_infos.exit_code)
+
 
 def _common_action(environment, region, configuration_file, configuration_dir, verbose, log_file):
     logger = _create_logger(verbose, log_file)
@@ -91,31 +103,34 @@ def _common_action(environment, region, configuration_file, configuration_dir, v
     canary_infos = CanaryReleaseInfos(parameters.environment, parameters.region, parameters.configuration_file, version_infos)
     return logger, canary_infos
 
+
 @main.command(help='undeploy the ECS service')
-@click.option('-e','--environment', required=True, default= 'stage', help='Environment to deploy.', show_default=True )
-@click.option('-r','--region', required=True, default= 'eu-west-3', help='Amazon Web Service region used to deploy ECS service.', show_default=True )
-@click.option('-f','--configuration-file', required=False, help='deployment configuration file.')
-@click.option('-d','--configuration-dir', required=False, help='directory to find the deployment configuration file.')
+@click.option('-e', '--environment', required=True, default='stage', help='Environment to deploy.', show_default=True)
+@click.option('-r', '--region', required=True, default='eu-west-3', help='Amazon Web Service region used to deploy ECS service.', show_default=True)
+@click.option('-f', '--configuration-file', required=False, help='deployment configuration file.')
+@click.option('-d', '--configuration-dir', required=False, help='directory to find the deployment configuration file.')
 @click.option('--verbose', is_flag=True, default=False, help='activate verbose log.')
 @click.option('--log-file', required=False, help='output log file result.')
 def undeploy(
-        environment, 
-        region, 
+        environment,
+        region,
         configuration_file,
-        configuration_dir, 
-        verbose, 
+        configuration_dir,
+        verbose,
         log_file):
     logger, canary_infos = _common_action(environment, region, configuration_file, configuration_dir, verbose, log_file)
     canary_infos.action = 'undeploy'
     canary_step = PrepareUnDeploymentStep(canary_infos, logger)
-    while (canary_step != None):
+    while (canary_step):
         canary_step = canary_step.execute()
     sys.exit(canary_infos.exit_code)
+
 
 @main.command(name='version', help='show CLI version')
 def version():
     version_infos = VersionInfos()
     print(f'{version_infos.description} - {version_infos.version}')
+
 
 def _create_logger(verbose, log_file):
     logger = logging.getLogger('ecs-crd')
@@ -128,15 +143,16 @@ def _create_logger(verbose, log_file):
         boto3.set_stream_logger(name='botocore', level=logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s : %(name)s : %(levelname)s :  %(message)s')
     else:
-        formatter= logging.Formatter('%(name)s : %(message)s')
+        formatter = logging.Formatter('%(name)s : %(message)s')
 
     if log_file:
-        logging.basicConfig( filename=log_file, level=logging.DEBUG)
+        logging.basicConfig(filename=log_file, level=logging.DEBUG)
 
     ch = logging.StreamHandler()
     ch.setFormatter(formatter)
     logger.addHandler(ch)
     return logger
+
 
 if __name__ == '__main__':
     main()
